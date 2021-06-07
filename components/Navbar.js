8c3199c42fe72f08e8ffbfffd039861692286e39
Lobo <matheuscardoso1@poli.ufrj.br>
2021-06-07 14:10:19 -0300

    small improvements in the Nav layout. Fixed the z-index and typography of the mobile menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,13 +3,6 @@ import Image from "next/image";
 import StyledSelect from "../components/StyledSelect";
 import React, { useState, useEffect } from "react";
 
-// const fetchJobs = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
-//   let data = res.json();
-
-//   console.log(data);
-// };
-
 export default function Navbar({ data }) {
   const [showResponsiveMenu, setShowResponsiveMenu] = useState(false);
   const [jobData, setJobData] = useState("");
@@ -64,39 +57,7 @@ export default function Navbar({ data }) {
             </div>
           </li>
         </div>
-        <div
-          // style={showResponsiveMenu ? { display: "flex" } : { display: "none" }}
-          className={`${
-            showResponsiveMenu ? styles.onScreen : styles.offScreen
-          }  ${styles["navbar__menu--mobile"]}`}
-        >
-          <li>
-            <StyledSelect
-              fontSize="1.3rem"
-              id="language_option"
-              name="language"
-              strong="true"
-              width="100%"
-            >
-              <option className={styles["navbar__select--option"]} value="pt">
-                🇧🇷 PT-BR
-              </option>
-              <option className={styles["navbar__select--option"]} value="en">
-                🇺🇸 EN
-              </option>
-            </StyledSelect>
-          </li>
-          <li>Vagas</li>
-          <li>Blog</li>
-          <li>
-            <div className={` ${styles["menu__login"]}`}>Logar</div>
-          </li>
-          <li>
-            <div className={` ${styles["menu_cadastrar"]}`}>
-              Cadastre uma vaga
-            </div>
-          </li>
-        </div>
+
         <li>
           <div
             onClick={responsiveMenuHandler}
@@ -105,6 +66,39 @@ export default function Navbar({ data }) {
             <div />
             <div />
             <div />
+          </div>
+        </li>
+      </div>
+      <div
+        // style={showResponsiveMenu ? { display: "flex" } : { display: "none" }}
+        className={`${
+          showResponsiveMenu ? styles.onScreen : styles.offScreen
+        }  ${styles["navbar__menu--mobile"]}`}
+      >
+        <li>
+          <StyledSelect
+            fontSize="1.3rem"
+            id="language_option"
+            name="language"
+            strong="true"
+            width="100%"
+          >
+            <option className={styles["navbar__select--option"]} value="pt">
+              🇧🇷 PT-BR
+            </option>
+            <option className={styles["navbar__select--option"]} value="en">
+              🇺🇸 EN
+            </option>
+          </StyledSelect>
+        </li>
+        <li>Vagas</li>
+        <li>Blog</li>
+        <li>
+          <div className={` ${styles["menu__login"]}`}>Logar</div>
+        </li>
+        <li>
+          <div className={` ${styles["menu_cadastrar"]}`}>
+            Cadastre uma vaga
           </div>
         </li>
       </div>
